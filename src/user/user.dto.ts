6e1fb7d4013106e15852e61107c82beb541cff22
tymzap <tymzap@gmail.com>
2024-10-17 20:17:52 +0200

    Exclude age and email from response

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,8 +1,14 @@
+import { Exclude } from 'class-transformer';
+
 export class UserDto {
   id: string;
   name: string;
   isActive: boolean;
   createdAt: string;
+
+  @Exclude()
   age: number;
+
+  @Exclude()
   email: string;
 }
